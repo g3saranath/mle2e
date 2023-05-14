fe7ffca6956cf6a822d8fd196e0dad7e500582ef
g3saranath <gayathrisaranath1998@gmail.com>
2023-05-14 19:16:54 +0800

    setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(path:str)->List[str]:
+    '''
+    This function will return list of requirements
+    '''
+    requirements=[]
+    with open(path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','')for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+name='MLE2E Project',
+version='0.0.1',
+author='Gayathri',
+author_email='gayathrisaranath1998@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+)
